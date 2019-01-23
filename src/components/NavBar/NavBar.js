81f1c2a8db1f8a4801cc74c2388f3bfd4749a73a
Grant Rygh <grantrygh@gmail.com>
2019-01-23 15:40:45 -0600

    reduced height of navbar and logo for smaller mobile devices

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,9 +8,7 @@ import ToggleDrawer from '../SideDrawer/ToggleDrawer/ToggleDrawer'
 const toolbar = (props) => (
 	<header className={classes.NavBar}>
 		<ToggleDrawer clicked={props.showMenu} />
-		<div>
-			<Logo />
-		</div>
+		<Logo />
 		<nav className={classes.DesktopOnly}>
 			<NavigationItems location="navBar" />
 		</nav>
